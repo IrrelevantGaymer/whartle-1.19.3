37aa6dac76b8d09e3447be2b8b4752e5028df62f
Cora River Maxwell <101901cora@gmail.com>
2023-01-02 23:22:01 -0800

    Initial commit

---
+++ src/main/java/net/IrrelevantGaymer/whartle/WhartleMod.java
@@ -0,0 +1,15 @@
+package net.IrrelevantGaymer.whartle;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhartleMod implements ModInitializer {
+	public static final String MOD_ID = "whartle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/IrrelevantGaymer/whartle/WhartleModClient.java
@@ -0,0 +1,10 @@
+package net.IrrelevantGaymer.whartle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhartleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
